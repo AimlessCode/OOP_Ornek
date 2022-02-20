b0486ea4caadd5e4effc7587b72fcba4476f3f6e
AimlessCode <emin_baycan@outlook.com>
2022-02-20 10:19:35 +0300

    OOP_Satis

--- src/com/emin/repository/MusteriRepository.java
+++ src/com/emin/repository/MusteriRepository.java
@@ -1,4 +1,15 @@
 package com.emin.repository;
 
 public class MusteriRepository {
+    /**
+     * Temel olarak verilerin depolandığı yerden alınarak
+     * istenilen şekilde sunulması yapar.
+     * 1- Herhangi bir depolama yeri olmadan hafıza veriler
+     * tutularak sanal verilerle çalışılabilir.
+     * 2- Geçici veritabanları veriler işlenere alınabilir
+     * 3- Sabit veritabanlarından bağlantı kurularak veriler
+     * alınır ve işlenerek sununlur.
+     *
+     */
+
 }
